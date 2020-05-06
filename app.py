3f9c7700a7a25dde97d3641fa8fffd126486e7b8
Hasham <m.hasham681@gmail.com>
2020-05-06 18:26:08 +0500

    added api, app V1
    Co-authored-by: Muhammad Asadullah <Asadullah007@users.noreply.github.com>
    Co-authored-by: Muhammad Hasham Khalid <Muhammad-Hasham-Khalid@users.noreply.github.com>

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request, make_response
+import requests
+
+app = Flask(__name__)
+
+# /api/virufy -> all data will be posted here and we will return the response
+
+@app.route('/app', methods=['POST'])
+def post():
+    print(request.form)
+    age = request.form.get('age')
+    gender = request.form.get('gender')
+    smoker = request.form.get('smoker')
+    symptoms = request.form.getlist('reported_symptoms')
+    medical_history = request.form.getlist('medical_history')
+    # cough audio = request.form.get('')
+    # breath audio = request.form.get('')
+    # finger video = request.form.get('')
+    patient_location = request.form.get('location')
+
+    response = {"age": age, "gender": gender,
+     "smoker": smoker, "reported_symptoms": symptoms,
+     "medical_history": medical_history,
+     "location": patient_location }
+
+    return make_response(jsonify(response), 200)
+
+app.run(port=5000, debug=True)
